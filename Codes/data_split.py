f9aad961f22e308a177d3df4a77feefa55075d19
K. Lakshmi Phalguni <38901852+klphalguni@users.noreply.github.com>
2018-05-04 19:25:16 +0530

    Update data_split.py

--- Codes/data_split.py
+++ Codes/data_split.py
@@ -1,13 +1,15 @@
 import sys
 sys.path.append("..\\libs")
 from classification import utils
+
 data_dir='F:\\ML_Project\\Data\\'
 sample_dir='F:\ML_Project\Data\\sample\\'
-#initialize the Dataset
+
+# Initializing the Dataset
 dataset=utils.Dataset(data_dir)
-# Split into Train, Val and Test Sets
+
+# Splitting into Training and Testing datasets
 dataset.split_train_test_val()
-# Generate the Sample Dataset
-dataset.gen_sample_set(sample_dir=sample_dir)
+
 # Print Dataset Statistics
 dataset.print_dataset_stats()
