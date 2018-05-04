eb3fcc26944a8da488546fb2a9cc82f9e07df7ba
K. Lakshmi Phalguni <38901852+klphalguni@users.noreply.github.com>
2018-05-04 19:35:32 +0530

    Update train_crnn.py

--- Codes/train_crnn.py
+++ Codes/train_crnn.py
@@ -51,11 +51,11 @@ background_volume=0.3
 learning_rate='0.0005,0.0001,0.00002' # Training with different learning rates for different iterations
 train_steps='10000,10000,10000'       # No. of iterations for each learning rate  
 batch_size=256
-model_size_info=[48, 10, 4, 2, 2, 2, 60, 84]
-
-#Initialise the get_train_data() get_val_data() and get_test_data() Function
+model_size_info=[48, 10, 4, 2, 2, 2, 60, 84]   # CRNN parameters
 
 train_dir=os.path.join(FLAGS.data_dir,'train','audio')
+
+# Preparing model
 model_settings = models.prepare_model_settings(
       len(input_data.prepare_words_list(FLAGS.wanted_words.split(','))),
       FLAGS.sample_rate, FLAGS.clip_duration_ms, FLAGS.window_size_ms,
@@ -64,6 +64,9 @@ audio_processor = input_data.AudioProcessor(
       train_dir, silence_percentage, unknown_percentage,
       FLAGS.wanted_words.split(','), FLAGS.validation_percentage,
       FLAGS.testing_percentage, model_settings,use_silence_folder=True)
+
+# Initializing the get_train_data() get_val_data() and get_test_data() functions
+
 def get_train_data(args):
     sess=args
     time_shift_samples = int((FLAGS.time_shift_ms * FLAGS.sample_rate) / 1000)
@@ -71,27 +74,25 @@ def get_train_data(args):
         batch_size, 0, model_settings,background_frequency,
         background_volume, time_shift_samples, 'training', sess)
     return train_fingerprints,train_ground_truth
+
 def get_val_data(args):
-    '''
-    Input: (sess,offset)
-    '''
     sess,i=args
     validation_fingerprints, validation_ground_truth = (
             audio_processor.get_data(batch_size, i, model_settings, 0.0,
                                      0.0, 0, 'validation', sess))
     return validation_fingerprints,validation_ground_truth
+
 def get_test_data(args):
-    '''
-    Input: (sess,offset)
-    '''
     sess,i=args
     test_fingerprints, test_ground_truth = audio_processor.get_data(
         batch_size, i, model_settings, 0.0, 0.0, 0, 'testing', sess)
     return test_fingerprints,test_ground_truth
+
 def main(_):
     sess=tf.InteractiveSession()
-    # Placeholders
-    fingerprint_size = model_settings['fingerprint_size']
+
+    # Initializing Placeholders
+          fingerprint_size = model_settings['fingerprint_size']
     label_count = model_settings['label_count']
     fingerprint_input = tf.placeholder(
       tf.float32, [None, fingerprint_size], name='fingerprint_input')
@@ -100,16 +101,15 @@ def main(_):
     set_size = audio_processor.set_size('validation')
     label_count = model_settings['label_count']
     
-    # Create Model
-    
-    logits, dropout_prob = models.create_model(
+    # Creating CRNN Model
+      logits, dropout_prob = models.create_model(
       fingerprint_input,
       model_settings,
       model_architecture,
       model_size_info=model_size_info,
       is_training=True)
     
-    #Start Training
+    # Start Training
     extra_args=(dropout_prob,label_count,batch_size,set_size)
     trainer.train(sess,logits,fingerprint_input,ground_truth_input,get_train_data,
                   get_val_data,train_steps,learning_rate,eval_step_interval, logging_interval=logging_interval,
